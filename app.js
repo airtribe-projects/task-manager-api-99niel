3cb09e45eee4e03a7b33bf10aa7ac38cdc29bb83
Nitish Bandagar <nitishbandagar@gmail.com>
2024-12-18 10:30:19 +0530

    Initial commit for Task Manager API

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -15,3 +16,27 @@ app.listen(port, (err) => {
 
 
 module.exports = app;
+=======
+require("dotenv").config();
+const express = require('express');
+const app = express()
+
+const tasksRouter = require("./routes/tasks")
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); // Middleware to parse JSON
+
+const logger = (req, res, next) => {
+   console.log(`${req.method}: Request Received On ${req.url}`);
+   next();
+};
+
+app.use(logger);
+console.log({ tasksRouter })
+app.use(tasksRouter);
+
+app.listen(PORT ,() => {
+   console.log("SERVER IS UP AND RUNNING ON PORT NO: " + PORT);
+});
+>>>>>>> 7e9e6a2 (Initial commit for Task Manager API)
